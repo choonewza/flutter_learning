1a30ce525b930c7e4f9bdf9b2b3d53bc93182bb3
choonewza <choonewza@gmail.com>
2020-05-12 11:07:58 +0700

    dart example

--- dart/ex4.dart
+++ dart/ex4.dart
@@ -1,18 +1,39 @@
-void main(){
+void main() {
+  Dog d1 = new Dog('BREED', 'Mike', 14, 35);
+  d1.bark("555");
 
+  SmallDog s1 = new SmallDog("POODLE", 'Brown');
+  s1.runAway();
+  print(s1.isSmall());
 }
 
-class Dog{
+class Dog {
   int age;
   String type;
   String name;
   int size;
 
-   Dog( String type, String name, [int age = 18,int size =10]) {
-     this.age = age;
-     this.type = type;
-     this.name = name;
-     this.size = size;
-   }
+  Dog(String type, String name, [int age = 18, int size = 10]) {
+    this.age = age;
+    this.type = type;
+    this.name = name;
+    this.size = size;
+  }
 
+  void bark(String text) {
+    print('หมาชื่อ $name พันธ์ $type อายุ $age เห่าว่า $text');
+  }
+}
+
+class SmallDog extends Dog {
+  static int MAX_BODY_SIZE = 30;
+
+  SmallDog(String type, String name, [int age = 18, int size = 10])
+      : super(type, name, age, size) {}
+
+  void runAway() {
+    print('I am running');
+  }
+
+  bool isSmall() => !(this.size > MAX_BODY_SIZE);
 }
